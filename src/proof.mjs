393b6272f810e710562fc520be473071766499d0
Graham Leach-Krouse <gleachkr@gmail.com>
2023-10-29 12:11:57 -0400

    Clean up initial commit
    * Move spacing logic into tree-nodes, for additional flexibility
    * Seperate out some code that should rerun on reconnect
    * General linting

--- src/proof.mjs
+++ src/proof.mjs
@@ -1,33 +1,41 @@
 const registry = window.customElements
 
 class Root extends HTMLElement {
-  connectedCallback() {
+  constructor() {
+    super()
     this.attachShadow({ mode: "open" })
-    const style = document.createElement("style")
-    style.textContent = `
-      .forest {
-        display: flex;
-        flex-wrap: nowrap;
-        flex-direction: row;
-        justify-content: center;
-        align-items: flex-end;
-      }
+  }
 
-      .tree, :host {
-        display: inline-flex;
-        flex-wrap: nowrap;
-        flex-direction: column;
-      }
+  connectedCallback() {
+    if (!this.initialized) {
+      const stylesheet = document.createElement("style")
+      stylesheet.textContent = `
+        .forest {
+          display: flex;
+          flex-wrap: nowrap;
+          flex-direction: row;
+          justify-content: center;
+          align-items: flex-end;
+        }
 
-      .inference {
-        position:absolute;
-        bottom:-.5em;
-        font-size:.6em;
-        margin: 0 .5em 0 .5em;
-      }
-    `
-    this.shadowRoot.appendChild(style);
-    [...this.children].map(child => this.shadowRoot.appendChild(child))
+        .tree, :host {
+          display: inline-flex;
+          flex-wrap: nowrap;
+          flex-direction: column;
+        }
+
+        .inference {
+          position:absolute;
+          bottom:-.5em;
+          font-size:.6em;
+          margin: 0 .5em 0 .5em;
+        }
+      `
+      this.shadowRoot.appendChild(stylesheet);
+      [...this.children].map(child => this.shadowRoot.appendChild(child))
+
+      this.initialized = true
+    }
   }
 }
 
@@ -39,89 +47,187 @@ class Tree extends HTMLElement {
   }
 
   connectedCallback() {
-    this.className = "tree"
-    if (this.initialized) return 
-    else this.initialized = true
-    const style = document.createElement("style")
-    console.log(this.parentElement.tagName)
-    style.textContent = `
+    if (!this.initialized) {
+      this.className = "tree"
+
+      const stylesheet = document.createElement("style")
+      stylesheet.textContent = ``
+
+      const nodeSlot = document.createElement("slot")
+      nodeSlot.setAttribute("name", "node")
+
+      const forestSlot = document.createElement("slot")
+      forestSlot.setAttribute("name", "forest")
+
+      this.shadowRoot.appendChild(stylesheet)
+      this.shadowRoot.appendChild(forestSlot)
+      this.shadowRoot.appendChild(nodeSlot)
+      this.initialized = true
+    }
+  }
+
+  // get the forest that this tree is in, if any
+  getForest() {
+    return this.parentElement?.tagName === "PROOF-FOREST"
+      ? this.parentElement
+      : null
+  }
+
+  getNextTree() {
+    let next = this.nextElementSibling
+    while (next) {
+      if (next.tagName === "PROOF-TREE") return next
+      next = next.nextElementSibling
+    }
+    return false
+  }
+
+  getPrevTree() {
+    let next = this.previousElementSibling
+    while (next) {
+      if (next.tagName === "PROOF-TREE") return next
+      next = next.previousElementSibling
+    }
+    return false
+  }
+}
+
+class Node extends HTMLElement {
+  constructor() {
+    super()
+    this.attachShadow({ mode: "open" })
+  }
+
+  connectedCallback() {
+    if (!this.initialized) {
+
+      this.className = "node"
+      this.setAttribute("slot", "node")
+
+      this.stylesheet = document.createElement("style")
+
+      const leftSpace = document.createElement("div")
+      leftSpace.className = "left-space"
+
+      const rightSpace = document.createElement("div")
+      rightSpace.className = "right-space"
+
+      this.content = document.createElement("div")
+      this.content.className = "content"
+
+      this.rightLabel = document.createElement("div")
+      this.rightLabel.className = "label"
+
+      this.leftLabel = document.createElement("div")
+      this.leftLabel.className = "label"
+
+      this.shadowRoot.appendChild(this.stylesheet)
+      this.shadowRoot.appendChild(leftSpace)
+      leftSpace.appendChild(this.leftLabel)
+      this.shadowRoot.appendChild(this.content)
+      this.shadowRoot.appendChild(rightSpace)
+      rightSpace.appendChild(this.rightLabel)
+
+      this.initialized = true
+    }
+
+    this.updateStyle()
+    this.updateLabel()
+    this.updateText()
+
+  }
+
+  //TODO: connect to mutation observer 
+  updateText() {
+    this.text = [... this.childNodes].filter(e => e.nodeType === Node.TEXT_NODE)
+    this.content.innerHTML = ""
+    this.text.map(textnode => this.content.appendChild(textnode.cloneNode()))
+  }
+
+  //TODO: connect to mutation observer on prooftree
+  updateLabel() {
+    const noNextTree = !this.getTree().getNextTree()
+
+    if (this.getConsumer() && noNextTree) {
+      this.rightLabel.innerText = this.getConsumer().innerText
+    }
+  }
+
+  //TODO: connect to mutation observer on proofforest
+  updateStyle() {
+    const inForest = this.getTree().getForest()
+    const noNextTree = !this.getTree().getNextTree()
+    const noPrevTree = !this.getTree().getPrevTree()
+
+    this.stylesheet.textContent = `
       .left-space, .right-space {
         flex-grow:1;
         min-width:.5em;
       }
 
       .right-space {
-        border-bottom: ${this.nextElementSibling ? "1px solid black" : "none"};
+        border-bottom: ${noNextTree ? "none" : "1px solid black"};
         position:relative;
       }
 
       .left-space {
-        border-bottom: ${this.previousElementSibling ? "1px solid black" : "none"};
+        border-bottom: ${noPrevTree ? "none" : "1px solid black"};
+        position: relative;
+      }
+
+      .right-space .label {
+        position:absolute;
+        font-size: .7em;
+        bottom: -.6em;
+        padding-left: .5em;
       }
 
       .content {
-        border-bottom: ${this.parentElement.tagName === "PROOF-ROOT" ?  "none" : "1px solid black"};
+        border-bottom: ${inForest ? "1px solid black" : "none"};
         padding:0 .5em 0 .5em;
       }
 
-      .base {
+      :host {
         display: flex;
         flex-wrap: nowrap;
         flex-direction: row;
         justify-content: center;
       }
-
     `
-    const leftSpace = document.createElement("div")
-    leftSpace.className = "left-space"
-
-    const content = document.createElement("div")
-    content.className = "content"
-
-    const rightSpace = document.createElement("div")
-    rightSpace.className = "right-space"
-
-    const base = document.createElement("div")
-    base.className = "base"
-
-    const nodeSlot = document.createElement("slot")
-    nodeSlot.setAttribute("name","node")
-
-    const inferenceSlot = document.createElement("slot")
-    inferenceSlot.setAttribute("name","inference")
-
-    const forestSlot = document.createElement("slot")
-    forestSlot.setAttribute("name","forest")
-
-    this.shadowRoot.appendChild(style)
-    this.shadowRoot.appendChild(forestSlot)
-    this.shadowRoot.appendChild(base)
-    base.appendChild(leftSpace)
-    base.appendChild(content)
-    base.appendChild(rightSpace)
-    rightSpace.appendChild(inferenceSlot)
-    content.appendChild(nodeSlot)
   }
-}
 
-class Node extends HTMLElement {
-  connectedCallback() {
-    this.className = "node"
-    this.setAttribute("slot","node")
+  // get the proof tree housing this node, if any
+  getTree() {
+    return this.parentElement?.tagName === "PROOF-TREE"
+      ? this.parentElement
+      : null
+  }
+
+  // get the inference that is consuming this node
+  getConsumer() {
+    return this.getTree()?.getForest()?.getInference()
   }
 }
 
 class Inference extends HTMLElement {
   connectedCallback() {
     this.className = "inference"
-    this.setAttribute("slot","inference")
+    this.setAttribute("slot", "inference")
+    this.style.display = "none"
   }
 }
 
 class Forest extends HTMLElement {
   connectedCallback() {
     this.className = "forest"
-    this.setAttribute("slot","forest")
+    this.setAttribute("slot", "forest")
+    const inferences = [...this.children].filter(el => el.tagName === "PROOF-TREE-INFERENCE")
+    if (inferences.length > 1) console.error("WARNING: Multiple inferences attached to proof forest", this)
+    this.inference = inferences[0]
+  }
+
+  getInference() {
+    return this.inference
   }
 }
 
